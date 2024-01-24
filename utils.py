e63bb096996e9015f01e7b15dbdcb8607792f8c7
crasgaitis <crasgaitis@gmail.com>
2024-01-23 19:44:28 -0800

    build df from csv

--- utils.py
+++ utils.py
@@ -53,3 +53,29 @@ def build_dataset(tracker, label, add_on = False, df_orig = pd.DataFrame(),
     
     else:
         return df, dict_list
+
+def build_dataset_from_csv(file_path, label):
+    '''
+    Builds a dataframe from a csv file. Revives tuples from string format.
+    file_path: path to csv file
+    label: label for the data
+    add_on: if True, add to existing dataframe
+    df_orig: original dataframe to add to
+
+    returns: dataframe with data from csv file
+    '''
+
+    # all the columns that are tuples
+    # TODO: compute programmatically
+    tuples = ['left_gaze_point_on_display_area',
+    'left_gaze_point_in_user_coordinate_system',
+    'left_gaze_origin_in_user_coordinate_system',
+    'left_gaze_origin_in_trackbox_coordinate_system',
+    'right_gaze_point_on_display_area',
+    'right_gaze_point_in_user_coordinate_system',
+    'right_gaze_origin_in_user_coordinate_system',
+    'right_gaze_origin_in_trackbox_coordinate_system']
+
+    df = pd.read_csv(file_path, converters={key: ast.literal_eval for key in tuples})
+    df['type'] = label
+    return df
