e4056e5b07306a43c0a19fc47fe2b48e49114ea3
Ruslana Korolov <rkorolov@icloud.com>
2024-02-13 20:15:51 -0800

    updated flask code

--- ui/flask_ui_script.py
+++ ui/flask_ui_script.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 import threading
 import eye_tracking
+from utils import build_dataset_from_csv, gaze_id
 
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode='eventlet')
@@ -12,9 +13,14 @@ def index():
 
 @socketio.on('get_eye_tracking_data')
 def get_eye_tracking_data():
-    eye_tracking_data = eye_tracking.eye_tracking_data
+
+    # eye_tracking_data = eye_tracking.eye_tracking_data
+    # get a dataframe from the build dataset function -> from csv just for testing
+    df = build_dataset_from_csv('cat_looking_updown.csv', 'cat')
+    data = gaze_id(df)
+    
     # print(eye_tracking_data)
-    socketio.emit('update_eye_tracking_data', {'data': eye_tracking_data})
+    socketio.emit('update_eye_tracking_data_', {'data': data})
 
 if __name__ == '__main__':
     # start the eye tracking script
