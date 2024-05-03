356aaace22f38ad43ee2b4c3eb7489498d52c0a2
crasgaitis <crasgaitis@gmail.com>
2024-05-02 18:20:07 -0700

    discrete version w corrected commands

--- ui/eye_tracking.py
+++ ui/eye_tracking.py
@@ -1,7 +1,7 @@
 import threading
 import serial
 import numpy as np
-from utils import get_tracker, gaze_data, gaze_id, preprocess_gaze, calculatePower_new
+from utils import get_tracker, gaze_data, gaze_id, preprocess_gaze, calculatePower_new2
 import tobii_research as tr
 import time
 
@@ -10,20 +10,35 @@ def gaze_data_callback(out):
         return
             
     gazexy = preprocess_gaze(out)
-    eye_tracking_data = gaze_id(gazexy)
+    # print(np.round(gazexy, 1))
+    # print('\n')
+    # time.sleep(0.5)
+    # eye_tracking_data = gaze_id(gazexy)
     
-    # stream to bytes
-    left, right = calculatePower_new(gazexy)
-    cmd = f"CMD: {left + 1.0},{right + 1.0}\n" # format request to controller
+    # # stream to bytes
+    left, right = calculatePower_new2(gazexy)
+    # left +=1
+    # right +=1
+    # right += (0.2) * (2/3)
+    # if right >= 2:
+    #     right = 2
+    #     right = np.round(right, 1)      
+    # cmd = f"CMD: {round(((left)), 2)},{round((right), 2)}\n" # format request to controller
+    # print(cmd)
+    # left = -2.0
+    # right = -2.0
+    cmd = f"CMD: {round(((left)), 2)},{round((right), 2)}\n" # format request to controller
+    print(cmd)
+
     car.write(cmd.encode())
     car.flush() # make sure it all sends before you start reading
-    print(f"Sent {left},{right}")
+    print(cmd)
     
 def update_eye_tracking_data():
     global car
 
     baud = 9600
-    bluetoothPort = "COM3"
+    bluetoothPort = "COM14"
     car = serial.Serial(bluetoothPort, baud)
 
     TRACKER = get_tracker()
@@ -36,7 +51,7 @@ def update_eye_tracking_data():
     except KeyboardInterrupt:
         TRACKER.unsubscribe_from(tr.EYETRACKER_GAZE_DATA, gaze_data_callback)
 
-        car.close()
+        car.close() 
 
 # Start a thread to continuously update eye tracking data
 if __name__ == '__main__':
